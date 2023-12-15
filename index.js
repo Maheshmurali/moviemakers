7e66e8d74b71be40c3e7b1557a5859b5f5081ec6
Mahesh M <maheshmuralimvk@gmail.com>
2023-12-15 19:18:39 +0530

    Upadte

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function darkMode(e){
     if (e.name =='night'){
         document.querySelector("#mainDiv").classList.remove("bg-slate-900");
         document.querySelector("#mainDiv").classList.remove("text-white");
-        toggleImage.src = "/moon.png"
+        toggleImage.src = "https://i.postimg.cc/cH9Cb5qd/moon.png"
         e.name = 'day';
 
     }else{
             document.querySelector("#mainDiv").classList.add("bg-slate-900");
             document.querySelector("#mainDiv").classList.add("text-white");
-            toggleImage.src = "/day.png"
+            toggleImage.src = "https://i.postimg.cc/CKpqGt0t/day.png"
             e.name = 'night';   
     }
 }
